dbe728a8450b7cae800bcf7060b51c8f617b10ef
Francois Brulotte <francois.brulotte@gmail.com>
2017-06-22 13:29:20 -0400

    check if TEAMCITY_SERVER env variable is set before running

--- docker-alpine-teamcity-agent/entrypoint.sh
+++ docker-alpine-teamcity-agent/entrypoint.sh
@@ -1,5 +1,13 @@
 #!/usr/bin/env sh
 
+# TEAMCITY_SERVER environment variable must be set
+if [ -z "$TEAMCITY_SERVER" ]; then
+    echo "TEAMCITY_SERVER environment variable must be set"
+    exit 1
+else
+    echo "TEAMCITY_SERVER set to $TEAMCITY_SERVER"
+fi
+
 if [ ! -d "bin" ]; then
     echo "Setting up TeamCity agent for the first time in $(pwd) with Teamcity Server $TEAMCITY_SERVER"
     wget $TEAMCITY_SERVER/update/buildAgent.zip
