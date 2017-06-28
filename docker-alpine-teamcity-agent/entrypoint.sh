bfb923e8899f9fd2f312ea36f7f9b803c6856b08
François Brulotte <fbrulotte@users.noreply.github.com>
2017-06-28 11:48:33 -0400

    keep buildAgent.properties if it already exists

--- docker-alpine-teamcity-agent/entrypoint.sh
+++ docker-alpine-teamcity-agent/entrypoint.sh
@@ -11,14 +11,19 @@ fi
 if [ ! -d "bin" ]; then
     echo "Setting up TeamCity agent for the first time in $(pwd) with Teamcity Server $TEAMCITY_SERVER"
     wget $TEAMCITY_SERVER/update/buildAgent.zip
-    unzip -q buildAgent.zip
+    unzip -qo buildAgent.zip
     rm buildAgent.zip
+else
+    echo "Using agent at $(pwd)"
+fi
 
+if [ ! -e "conf/buildAgent.properties" ]; then
+    echo "Creating conf/buildAgent.properties"
     cp conf/buildAgent.dist.properties conf/buildAgent.properties
     sed -i "s/serverUrl=.*/serverUrl=$(echo $TEAMCITY_SERVER | sed -e 's/[\/&]/\\&/g')/" conf/buildAgent.properties
     chmod +x bin/agent.sh
 else
-    echo "Using agent at $(pwd)"
+    echo "Using existing conf/buildAgent.properties"
 fi
 
 bin/agent.sh run
